be6621af88d1348505b1c245ec05ccaec6d35d1d
pinkyun <pinkyun@kimokyun>
2018-01-17 09:13:25 -0800

    두수의 합

--- src/okyoun/calendar/Calendar.java
+++ src/okyoun/calendar/Calendar.java
@@ -1,15 +1,20 @@
 package okyoun.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("--------------");
-		System.out.println("1 2 3 4 5 6 7");
-		System.out.println("8 9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("두수를 입력하세요");
+		Scanner scanner=new Scanner(System.in);
+		int first=scanner.nextInt();
+		int second=scanner.nextInt();
+		int result=first+second;
+		//System.out.println("두수의 합은"+result+"입니다");
+		System.out.printf("두수의 합은%d입니다",result);
+		scanner.close();
+
 	}
 
 }
