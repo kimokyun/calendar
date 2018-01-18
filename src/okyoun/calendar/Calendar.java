d03729a7e956935328fdd596469c68932ba0282c
pinkyun <pinkyun@kimokyun>
2018-01-18 07:51:52 -0800

    반복으로 달의 마지막일 구하기

--- src/okyoun/calendar/Calendar.java
+++ src/okyoun/calendar/Calendar.java
@@ -8,18 +8,35 @@ public class Calendar {
 	public int maxDaysOfMonth(int month) {
 		return MAX_DAY[month-1];
 	}
+	public void getMonth(int num) {
+		Scanner s=new Scanner(System.in);
+		for(int i=0;i<num;i++) {
+			System.out.println("월을 입력");
+			int month=s.nextInt();
+			System.out.println(MAX_DAY[month]);
+		}	
+	s.close();
+	
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner s=new Scanner(System.in);
 		Calendar cal=new Calendar();
+		
+		
+		System.out.println("반복횟수를 입력");
 		int num=s.nextInt();
-		if(num<1||num>12)
-			System.out.println("1~12사이의 숫자를 입력해줘");
-		else {
-			System.out.println(num+"월은"+cal.maxDaysOfMonth(num)+"까지 있습니다.");
-		}
+		cal.getMonth(num);
+		System.out.println("끝");
+		
+//		if(num<1||num>12)
+//			System.out.println("1~12사이의 숫자를 입력해줘");
+//		else {
+//			System.out.println(num+"월은"+cal.maxDaysOfMonth(num)+"까지 있습니다.");
+//		}
 		s.close();
 
 	}
